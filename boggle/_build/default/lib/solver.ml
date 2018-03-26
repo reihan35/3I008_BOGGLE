5707f3eb07792978a3cfd48c873e7e7e8d3545d9
Fati <reihaneh77@yahoo.com>
2018-03-26 20:56:07 +0200

    find_all_paths compiled

--- boggle/_build/default/lib/solver.ml
+++ boggle/_build/default/lib/solver.ml
@@ -13,4 +13,5 @@ let rec backtrack board lexicon path (i, j) =
                  |false->Iter.append (Iter.empty)  (Iter.flat_map (fun x -> backtrack board lex_prime path_prime x) (Board.neighbours board (i, j)));;
 
 let find_all_paths board lexicon =
-  failwith "Unimplemented"
+  (*failwith "unimplemented"*)
+  Iter.flat_map (fun x -> backtrack board lexicon Path.empty x) (Board.all_positions board);;
