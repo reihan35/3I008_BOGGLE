adbf9339eaddaf52fd0d0db44653ec542964c3bc
laudao <nguyen.laura97@gmail.com>
2018-03-28 19:22:06 +0200

    project hopefully finished

--- boggle/_build/default/lib/lexicon.ml
+++ boggle/_build/default/lib/lexicon.ml
@@ -11,7 +11,6 @@ type t = {
   words : t M.t;
 }
 
-(* let iter_sur_mot = ref Iter.empty *)
 
 let empty =
   (* Cette valeur vous est donnée, vous n'avez pas besoin de l'écrire
@@ -22,12 +21,10 @@ let empty =
   }
 
 let rec is_empty { eow; words } =
-  (* failwith "Unimplemented" *)
   eow==false && M.for_all (fun k t -> is_empty t) words
 
 
 let add lexicon word =
-  (* iter_sur_mot :=(Iter  .cons word (!iter_sur_mot)); *)
   let rec traverse n t =
      if n < String.length word then
       if M.mem word.[n] t.words then
@@ -36,38 +33,13 @@ let add lexicon word =
           {eow=t.eow;words= (M.add word.[n] (traverse (n+1) (M.find word.[n] t.words)) new_d)}
       else
         {eow=t.eow;words= (M.add word.[n] (traverse (n+1) empty) (t.words))}
-        (* {eow=t.eow;words=(traverse (n+1) (M.add word.[n] empty (t.words)))} *)
     else
         {eow=true;words=(M.add (String.get " " 0) {eow=false;words=M.empty} t.words)}
-(*        {eow=false;words=M.empty} *)
   in traverse 0 lexicon
 
-(*
-let add lexicon word =
-  let rec traverse n t =
-    if n < (String.length word) then
-      if M.mem word.[n] t.words then
-        let new_d = M.remove word.[n] t.words
-        in
-          {eow=t.eow;words= (M.add word.[n] (traverse (n+1) (M.find word.[n] t.words)) new_d)}
-      else
-        {eow=t.eow;words= (M.add word.[n] (traverse (n+1) empty) (t.words))}
-        (* {eow=t.eow;words=(traverse (n+1) (M.add word.[n] empty (t.words)))} *)
-    else
-      {eow=true;words=(M.add (String.get "" 0) {eow=true;words=M.empty} t.words)}
-  in traverse 0 lexicon *)
 
-(*
-let rec to_iter { eow; words } =
-  match words with
-  | w when w = M.empty ->
-      if eow then
-        Iter.singleton ""
-      else
-        Iter.empty
-  | w when eow -> Iter.cons "" (Iter.flat_map (fun (k,t) -> Iter.map (fun s -> (String.make 1 k) ^ s) (to_iter t)) (M.to_iter w) )
-  | w ->  Iter.flat_map (fun (k,t) -> Iter.map (fun s -> (String.make 1 k) ^ s) (to_iter t)) (M.to_iter w)
-*)
+
+
 let rec to_iter { eow; words } =
   match words with
   | w when w = M.empty -> Iter.singleton " "
@@ -76,46 +48,19 @@ let rec to_iter { eow; words } =
 
 
 let letter_suffixes { eow; words } letter =
-  if M.mem letter words then (* le dictionnaire contient la lettre *)
-      M.find letter words (* on retourne l'arbre qui correspond *)
+  if M.mem letter words then
+      M.find letter words
   else empty
 
-(*
-  let rec traverse t =
-    if t.words != M.empty then (* dictionnaire non vide *)
-      if M.mem letter t.words then (* le dictionnaire contient la lettre *)
-        M.find letter t.words (* on retourne l'arbre qui correspond *)
-        (* {eow=false;words=t.words} *)
-      else
-        (* on appelle traverse sur les arbres du dictionnaire et on récupère celui dont la clé est la lettre *)
-        let m = M.filter (fun k x -> x.words != M.empty) (M.map (fun x -> traverse x) t.words) in
-        if (M.is_empty m) then
-          empty
-        else
-          let (k, x) = M.min_binding m
-        in x
-    else
-      empty
-  in traverse { eow; words } *)
 
 let rec filter_min_length len { eow; words } =
   let mots = (Iter.filter (fun x -> if String.length(x) > len || String.length(x) = len then true else false)(to_iter { eow; words })) in
       Iter.fold add empty mots
 
-(* let has_empty_word { eow; words } =
-   (Iter.exists (fun x -> if String.length(x) = 0 then true else false)(to_iter { eow; words })) *)
-
-(*
-let has_empty_word { eow; words } =
-   (Iter.exists (fun x -> if x = " " then true else false)(to_iter { eow; words })) *)
 
 let has_empty_word { eow; words } =
   M.mem " ".[0] words
 
-	(*let mots = (Iter.filter (fun x -> if String.length(x) > len then true else false)(to_iter { eow; words })) in
-    let lex = ref empty in
-      Iter.map (fun x -> add x !lex) mots;
-  !lex*)
 
 let load_file f =
   let rec load_channel channel acc =
--- boggle/bin/main.ml
+++ boggle/bin/main.ml
@@ -1,101 +1,71 @@
 open Boggle
-(*
+
 let main () =
-  if Array.length Sys.argv >= 2
-  then begin
+  if Array.length Sys.argv < 2 then
+    begin
+    print_string "********************************************************************************************\n";
+    print_string "Welcome to the Boggle Game !! Here's a Boggle generated for you ! Try to solve it by your own\n";
+    let random_letter_fr = RandomLetter.picker RandomLetter.Distribution.fr in
+    let board = Board.make 4 (random_letter_fr)
+    in
+    Board.print board;
+    print_string "\n";
+    print_string "\nOr tap enter to see the solutions if you are lazy !\n";
+    read_line();
+    print_string "Here's your Boggle's solutions :\n";
+    print_string "\n";
+    let dict = Lexicon.load_file "dict/dico_fr.txt" in
+    match dict with
+    |None->print_string "problem";
+    |Some d->
+    let my_compare x y=
+      if String.length x > String.length y then -1
+      else
+      if String.length x < String.length y then 1
+      else
+       String.compare x y;
+      in
+
+    let list_mots = List.sort_uniq my_compare (Iter.to_rev_list (Path.iter_to_words board (Solver.find_all_paths board d))) in
+    let rec parcours_liste l =
+	  match l with
+	  | [] -> print_string("\n");print_string "********************************************************************************************\n";
+	  | t::q -> print_string(t ^ " "); parcours_liste q
+    in parcours_liste list_mots;
+    end
+  else
+  begin
     print_string "J'ai reçu le paramètre ";
     print_endline Sys.argv.(1);
-  end
-  else begin
-    print_endline "Je n'ai reçu aucun paramètre"
+    print_string "********************************************************************************************\n";
+    print_string "Welcome to the Boggle Game !! Here's a Boggle generated for you ! Try to solve it by your own\n";
+    let b = Board.from_string Sys.argv.(1) in
+    match b with
+    |None->print_string "Sorry board length not valid "
+    |Some(board)->Board.print board;
+    print_string "\n";
+    print_string "\nOr tap enter to see the solutions if you are lazy !\n";
+    read_line();
+    print_string "Here's your Boggle's solutions :\n";
+    print_string "\n";
+    let dict = Lexicon.load_file "dict/dico_fr.txt" in
+    match dict with
+    |None->print_string "problem";
+    |Some d->
+    let my_compare x y=
+      if String.length x > String.length y then -1
+      else
+      if String.length x < String.length y then 1
+      else
+       String.compare x y;
+      in
+
+    let list_mots = List.sort_uniq my_compare (Iter.to_rev_list (Path.iter_to_words board (Solver.find_all_paths board d))) in
+    let rec parcours_liste l =
+	  match l with
+	  | [] -> print_string("\n");print_string "********************************************************************************************\n";
+	  | t::q -> print_string(t ^ " "); parcours_liste q
+    in parcours_liste list_mots
   end;
-  print_endline "Je ne sais pas quoi faire... Programmez moi !"
-*)
-
-let main () =(*"eibnrefclmmuerkpseeniagecewieiehusisevelumiieurerllcommssiuseelq"*)
-  print_string "***********************************************************************************\n";
-  print_string "Welcome to the Boggle Game !! Here's a Boggle generated for you ! Try to solve it by your own\n";
-  let random_letter_fr = RandomLetter.picker RandomLetter.Distribution.fr in
-  let board = Board.make 4 random_letter_fr
-  in
-  Board.print board;
-  print_string "\n";
-  print_string "\nOr tap enter to see the solutions if you are lazy !\n";
-  read_line();
-  print_string "Here's your Boggle's solutions :\n";
-  let dict = Lexicon.load_file "dict/dico_fr.txt" in
-  match dict with
-  |None->print_string "problem";
-  |Some d->
-(*   Iter.iter (fun x -> print_string x )(Lexicon.to_iter d);; *)
-(*  if Lexicon.has_empty_word d then print_string "il y a le mot vide\n" else print_string "il n'y a pas le mot vide";; *)
-
-(*   Iter.iter (fun x -> print_string ((Path.to_string board x)^"")) (Solver.backtrack board d Path.empty (0,3));; *)
-(*   let it_mots = Iter.iter (fun x -> print_string (x^" ")) (Path.iter_to_words board (Solver.find_all_paths board d)) in *)
-  let my_compare x y=
-    if String.length x > String.length y then -1
-    else
-    if String.length x < String.length y then 1
-    else
-     String.compare x y;
-    in
-
-  let list_mots = List.sort_uniq my_compare (Iter.to_rev_list (Path.iter_to_words board (Solver.find_all_paths board d))) in
-  let list_f = List.map (fun x -> match x with
-                                  |x when String.length x > 3 -> x) list_mots in
-  let rec parcours_liste l =
-	match l with
-	| [] -> print_string("\n")
-	| t::q -> print_string(t ^ " "); parcours_liste q
-  in parcours_liste list_f;;
-
-  (*let board = Board.from_string "abcd" in
-    match board with
-    | None -> print_string("Désolées pas carré")
-    | Some(t) -> Board.print t;
-                 let p = Path.add_tile t Path.empty (0,0) in
-                         match p with
-                           |None->print_string("erreur")
-                           |Some(c)->let g = Path.add_tile t c (0,1) in
-                                     match g with
-                                     |None -> print_string("problem");
-                                     |Some(b)->
-                                             let h = Path.add_tile t b (1,0) in
-                                               match h with
-                                              |Some(hi) -> print_string (Path.to_string t (Path.rev hi));
-                                              |None -> print_string("problem");;*)
-
-
-  (*let l = Lexicon.empty in
-  if Lexicon.is_empty l then print_string "vide\n" else print_string "non vide";
-  let a = Lexicon.add l "hugo" in
-  let b = Lexicon.add a "wyborska" in
-  let d = Lexicon.add b "wyb" in
-  let c = Lexicon.add d "wyborskal" in
-  let t = Lexicon.letter_suffixes c 'y' in
-  let t_s = Lexicon.to_iter t in*)
-
-
-  (*match open_in "dict/dico_fr_petit.txt" with
-  | channel -> print_string "works"
-  | exception Sys_error _ -> print_string "does not work";;
-  let v = Lexicon.load_file "dict/dico_fr.txt" in
-  match v with
-  |Some a->
-        print_string "voici la liste des mots de length 16 et plus\n";
-        let e = Lexicon.filter_min_length 16 a in
-        Iter.iter (fun x -> print_string(x^" ")) (Lexicon.to_iter e);
-        print_string "\n";
-        let z = Lexicon.letter_suffixes a 'w' in
-        print_string "voici la liste des mots qui start par w sans le w ! \n";
-        Iter.iter (fun x -> print_string(x^" ")) (Lexicon.to_iter z);
-  |None->print_string "problem";;*)
-
-
-(*if Lexicon.has_empty_word c then print_string "il y a le mot vide\n" else print_string "il n'y a pas le mot vide";;*)
-  (*let s = Lexicon.to_iter c in*)
-  (*if Lexicon.is_empty a then print_string "vide\n" else print_string "non vide";;*)
-  (*Iter.iter (fun x -> print_string(x^" ")) s;*)
-  (*Iter.iter (fun x -> print_string(x^" ")) t_s;;*)
-
-let () = main ()
+  print_endline "Bye Bye ! <____> "
+let () = main ();;
